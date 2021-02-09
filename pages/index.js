9e0cb61c9468e6d86e34139f2e3c23c88be61bc6
wong.jay <wong.jay@wongjaydeMacBook-Pro.local>
2021-02-09 20:54:54 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,171 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {
+  Box,
+  Heading,
+  Flex,
+  Image,
+  Input,
+  HStack,
+  Button,
+  Text,
+  InputGroup,
+  InputLeftElement,
+  PhoneIcon,
+  Avatar,
+  Link,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react'
+import { css, jsx } from '@emotion/react'
+import { AiOutlineSearch, AiOutlinePlus } from 'react-icons/ai'
+import React, { useState } from 'react'
 
+const InputCss = css`
+  border-color: #dfe1e5;
+  border-radius: 23px;
+  height: 46px;
+  width: 100%;
+  box-shadow: none;
+  padding: 0 30px 0 45px;
+  &:focus {
+    border: none;
+    box-shadow: 0 1px 6px rgba(223, 225, 229, 0);
+  }
+`
+
+const AvatarCss = css`
+  height: 50px;
+  width: 50px;
+  border-radius: 50px;
+  background-color: #f3f3f3;
+  text-align: center;
+  line-height: 50px;
+  display: inline-block;
+  cursor: pointer;
+`
 export default function Home() {
+  const data = [
+    { name: 'GitHub', path: 'https://github.com' },
+    { name: 'GitHub', path: 'https://github.com' },
+    { name: 'GitHub', path: 'https://github.com' },
+    { name: 'GitHub', path: 'https://github.com' },
+    { name: 'GitHub', path: 'https://github.com' },
+    { name: 'GitHub', path: 'https://github.com' },
+  ]
+  const [collectList, setCollectList] = useState([])
+  const [name, setName] = useState('')
+  const [path, setPath] = useState('')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const initialRef = React.useRef()
+  const finalRef = React.useRef()
+  const addList = () => {
+    setCollectList(
+      collectList.push({
+        name,
+        path,
+      })
+    )
+  }
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Box w="500px" mx="auto" mt="100px" overflow="hidden">
+        <Flex w="500px" justifyContent="center">
+          <Image src="/logo.png" w="320px" h="112px"></Image>
+        </Flex>
+        <Flex mt="20px">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              left="17px"
+              top="14px"
+              color="#9aa0a6"
+              fontSize="18px"
+              children={<AiOutlineSearch />}
+            />
+            <Input type="text" css={InputCss} />
+          </InputGroup>
+        </Flex>
+        <Flex mt="20px" flexWrap="wrap">
+          {data.map(item => (
+            <Box
+              key={item.path}
+              href={item.path}
+              w="100px"
+              textAlign="center"
+              flexShrink="0"
+            >
+              <Box css={AvatarCss}>
+                <Link href={item.path} isExternal>
+                  <Avatar
+                    src={`${item.path}/favicon.ico`}
+                    p="10px"
+                    maxH="50px"
+                  />
+                </Link>
+              </Box>
+              <Text fontSize="12px">{item.name}</Text>
+            </Box>
+          ))}
+          <Box w="100px" textAlign="center" flexShrink="0">
+            <Box css={AvatarCss} onClick={onOpen}>
+              <AiOutlinePlus />
+            </Box>
+            <Text fontSize="12px">添加快捷方式</Text>
+          </Box>
+        </Flex>
+      </Box>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>添加快捷方式</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>名称</FormLabel>
+              <Input
+                ref={initialRef}
+                placeholder="名称"
+                value={name}
+                onChange={ev => setName(ev.target.value)}
+              />
+            </FormControl>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            <FormControl mt={4}>
+              <FormLabel>网址</FormLabel>
+              <Input
+                placeholder="网址"
+                value={path}
+                onChange={ev => setPath(ev.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              完成
+            </Button>
+            <Button onClick={onClose}>取消</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   )
 }
